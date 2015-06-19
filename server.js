ce43d87cc5a15119e6dc269fa842e7310f8ecaa2
sukso96100 <sukso96100@gmail.com>
2015-06-20 00:18:06 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require("http");
+
+function start(){
+    function onRequest(request, response) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+}
+
+exports.start = start;
