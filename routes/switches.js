038e5a091c7a625c8329f5f3ff7262fd9c7b6ea9
sukso96100 <sukso96100@gmail.com>
2015-07-05 14:07:01 +0900

    working on settings page

--- routes/switches.js
+++ routes/switches.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+//DB
+var mongoose = require( 'mongoose' );
+var SwitchesConfig = mongoose.model( 'SwitchesConfig' );
 
-/* GET home page. */
+/* GET Settings page. */
 router.get('/', function(req, res, next) {
-  res.render('switches', { title: 'RelaySwitch - Settings' });
+  //Query Config
+  console.log("Querying Config from DB");
+  var query = SwitchesConfig.find().limit(8);
+  query.exec(function (err, config) {
+    if (err) return handleError(err);
+    console.log(config);
+    config = sortByKey(config,'num');
+    res.render('index', { title: 'RelaySwitch - Settings', state: config });
+  })
+
 });
 
 module.exports = router;
