c170233e56312ea997b4051d48017712f2a7055d
sukso96100 <sukso96100@gmail.com>
2015-07-05 12:51:55 +0900

    update

--- models/model.js
+++ models/model.js
@@ -1,11 +1,18 @@
 var mongoose = require('mongoose')
-var Schema = mongoose.Schema;
-
+//Connect to MongoDB
+console.log("Opening DB Connection");
+mongoose.connect( 'mongodb://localhost/relay-switch' );
+//Open DB Connection
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  // yay!
+  console.log("Yay! Opened DB Connection!");
+});
+console.log("Registering DB Schema : SwitchesConfig");
 var SwitchesConfigSchema = mongoose.Schema({
-    num: Number;
-    name: String
+    num: Number,
+    name: String,
     state: Boolean
 });
-
 mongoose.model('SwitchesConfig', SwitchesConfigSchema);
-mongoose.connect( 'mongodb://localhost/relay-switch' );
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,21 @@ var SwitchesConfig = mongoose.model( 'SwitchesConfig' );
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var query = SwitchesConfig.findOne({ 'num': 1 });
+  //Check if DB Table is empty
+  SwitchesConfig.find({num : 0}, function (err, docs) {
+        if (docs.length){
+            console.log("DB Table is NOT empty!");
+        }else{
+          console.log("DB Table is EMPTY!");
+          //Create Default Config
+          for(var i=0;i<8;i++){
+            var config0 = new SwitchesConfig({ num:i, name:"Switch "+i, state:false });
+            config0.save(function (err, config) {
+              if (err) return console.error(err);
+            });
+          }
+        }
+    });
 
   res.render('index', { title: 'RelaySwitch' });
 });
