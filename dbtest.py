3fd639fe64a9322d304498417730309cdf254270
sukso96100 <sukso96100@gmail.com>
2015-07-18 16:31:57 +0900

    update

--- dbtest.py
+++ dbtest.py
@@ -2,19 +2,26 @@ import logging
 import sys
 import time
 
-from peewee import *
-
+# from peewee import *
+import sqlite3
+db = sqlite3.connect('config.db')
+cursor = db.cursor()
 print("db Test")
+for row in cursor.execute('SELECT * FROM config ORDER BY NUM'):
+        print row
 
-db = SqliteDatabase('config.db', threadlocals=True)
 
 
-class config(Model):
-    NUM = IntegerField()
-    NAME = CharField()
-    STATE = BooleanField()
 
-    class Meta:
-        database = db
-for item in config.select():
-    print(item.NAME)
+# db = SqliteDatabase('config.db', threadlocals=True)
+#
+#
+# class config(Model):
+#     NUM = IntegerField()
+#     NAME = CharField()
+#     STATE = BooleanField()
+#
+#     class Meta:
+#         database = db
+# for item in config.select():
+#     print(item.NAME)
