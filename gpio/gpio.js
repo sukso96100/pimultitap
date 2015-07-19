110076769907e53556f2b5b4b48a0e59170ac78b
sukso96100 <sukso96100@gmail.com>
2015-07-19 10:24:09 +0900

    update

--- gpio/gpio.js
+++ gpio/gpio.js
@@ -1,10 +1,18 @@
 var Gpio = require('onoff').Gpio;
 var gpios = [];
 
-for(var i=2; i<9; i++){
+for(var i=2; i<10; i++){
   gpioItem = new Gpio(i,'out');
   gpios.push(gpioItem);
   console.log("setting up gpio"+i);
+}
+for(var i=0; i<gpios.length; i++){
+  gpios[i].writeSync(1);
+  console.log("[on]testing gpio"+i);
+}
+for(var i=0; i<gpios.length; i++){
+  gpios[i].writeSync(0);
+  console.log("[off]testing gpio"+i);
 }
 console.log(gpios);
 module.exports = gpios;
